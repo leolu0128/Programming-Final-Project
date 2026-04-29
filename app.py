3ee4169f2e66109850f47bb8836e925dde7109f5
Leoard <leoasdasd608@gmail.com>
2026-04-29 13:26:49 +0800

    Fix: Rename flask.py to app.py to avoid import error

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>這是我們的期末專題網站</h1><p>目前運行中！</p>"
+    return "<h1>這是我的期末專題網站</h1><p>目前運行中！</p>"
 
 if __name__ == '__main__':
     app.run(debug=True)
