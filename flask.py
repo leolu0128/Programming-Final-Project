5fd0a21e675b415b96c124c7ca4b267a2f49ef9e
Leoard <leoasdasd608@gmail.com>
2026-04-29 13:15:07 +0800

    Add Flask basic structure

---
+++ flask.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>這是我們的期末專題網站</h1><p>目前運行中！</p>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
